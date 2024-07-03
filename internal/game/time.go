5547cf7e814a7cbee09be77b100fbc795f89e01d
Joseph Naberhaus <contact@naberhausj.com>
2024-07-02 23:40:01 -0500

    Increase filter time range (#8)

--- internal/game/time.go
+++ internal/game/time.go
@@ -6,8 +6,8 @@ func PuzzleTimeRange() (time.Time, time.Time) {
 	// Center the games around Central Time (because that's where I live).
 	gmtNow := time.Now().In(time.FixedZone("CT", 0))
 
-	// End with commits from a year ago.
-	startDate := time.Date(gmtNow.Year()-1, gmtNow.Month(), gmtNow.Day(), 0, 0, 0, 0, gmtNow.Location())
+	// End with commits from 1.5 years ago so that we likely get authors that people remember.
+	startDate := time.Date(gmtNow.Year()-1, gmtNow.Month()-6, gmtNow.Day(), 0, 0, 0, 0, gmtNow.Location())
 	// End with commits from a week ago to increase the odds that our user will have an up-to-date history.
 	endDate := time.Date(gmtNow.Year(), gmtNow.Month(), gmtNow.Day()-7, 0, 0, 0, 0, gmtNow.Location())
 
